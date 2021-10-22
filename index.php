f0317918c4341336eb1ac91e931ab62416c61bdc
rail.mingaliev <rail.mingaliev@dn.ru>
2021-10-22 19:30:13 +0300

    первое занятие php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo 'Hello, ';
+echo "World!<br>";
+
+echo 'I\'m 28 years old<br>';
+
+echo '<button>Кнопка</button><br>';
+
+echo 'Hello, ' . 'World!<br>';
+
+echo 'Qwerty ' . '!<br>';
+
+echo '!' . trim(' Qwerty ') . '!<br>';
+
+echo 'MINGALIEV<br>';
+echo strtolower('MINGALIEV<br>');
+
+echo 'lowercase<br>';
+echo strtoupper('lowercase<br>');
+
+echo 'mingaliev rail<br>';
+echo ucwords('mingaliev rail<br>');
+
+echo strlen('Qwerty ') . '<br>';
+echo strlen('Qwerty') . '<br>';
---
+++ numbers.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 5 + 1;
+echo '<br>';
+echo 10 - 2;
+echo '<br>';
+echo 2 * 3;
+echo '<br>';
+echo 10 / 5;
+echo '<br>';
+echo 2 ** 3;
+echo '<br>';
+echo 12 % 3;
+
+echo 2 + (3 * 5);
